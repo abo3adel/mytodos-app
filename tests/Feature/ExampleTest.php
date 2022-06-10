2af12ebd1b0cae64fb9ee19476b339d3a4b1e90a
abo3adel <abo3adel35@gmail.com>
2022-06-10 23:41:25 +0100

    install pest test framework

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,17 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+uses(TestCase::class, RefreshDatabase::class);
 
-        $response->assertStatus(200);
-    }
-}
+it('it is true', function() {
+    expect(true)->toBe(true);
+});
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,11 @@
 <?php
 
-namespace Tests\Unit;
+// namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_that_true_is_true()
-    {
-        $this->assertTrue(true);
-    }
-}
+uses(TestCase::class);
+
+it('it is true', function() {
+    expect(true)->toBe(true);
+});
