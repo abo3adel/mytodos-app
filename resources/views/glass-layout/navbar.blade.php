bb89630b30f7de268f112434b75d2dd188e0c859
abo3adel <abo3adel35@gmail.com>
2022-06-18 00:24:22 +0100

    styling navbar

--- resources/views/glass-layout/navbar.blade.php
+++ resources/views/glass-layout/navbar.blade.php
@@ -1,26 +1,46 @@
 <div class="menu-circle"></div>
 <div class="header-menu">
+    <a class="menu-link d-md-none" href="#">
+        @include('icons.view-list')
+    </a>
     <a class="menu-link is-active" href="#">Apps</a>
-    <a class="menu-link notify" href="#">Your work</a>
+    {{-- <a class="menu-link notify" href="#">Your work</a> --}}
     <a class="menu-link" href="#">Discover</a>
-    <a class="menu-link notify" href="#">Market</a>
-</div>
-<div class="search-bar">
-    <input type="text" placeholder="Search">
+    {{-- <a class="menu-link notify" href="#">Market</a> --}}
 </div>
 <div class="header-profile">
-    <div class="notification">
-        <span class="notification-number">3</span>
-        <svg viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-            stroke-linejoin="round" class="feather feather-bell">
-            <path d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9M13.73 21a2 2 0 01-3.46 0" />
-        </svg>
-    </div>
-    <svg viewBox="0 0 512 512" fill="currentColor">
-        <path
-            d="M448.773 235.551A135.893 135.893 0 00451 211c0-74.443-60.557-135-135-135-47.52 0-91.567 25.313-115.766 65.537-32.666-10.59-66.182-6.049-93.794 12.979-27.612 19.013-44.092 49.116-45.425 82.031C24.716 253.788 0 290.497 0 331c0 7.031 1.703 13.887 3.006 20.537l.015.015C12.719 400.492 56.034 436 106 436h300c57.891 0 106-47.109 106-105 0-40.942-25.053-77.798-63.227-95.449z" />
-    </svg>
-    <img class="profile-img"
-        src="http://images.test/users/2.jpg"
-        alt="">
+    <!-- Authentication Links -->
+    @guest
+        @if(Route::has('login'))
+            {{-- <li class="nav-item"> --}}
+                <a class="nav-link"
+                    href="{{ route('login') }}">{{ __('Login') }}</a>
+            {{-- </li> --}}
+        @endif
+
+        @if(Route::has('register'))
+            {{-- <li class="nav-item"> --}}
+                <a class="nav-link"
+                    href="{{ route('register') }}">{{ __('Register') }}</a>
+            {{-- </li> --}}
+        @endif
+    @else
+        {{-- <li class="nav-item dropdown"> --}}
+            <a id="navbarDropdown" class="" href="#" role="button" data-bs-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false" v-pre>
+                <img class="profile-img" src="http://images.test/users/2.jpg" alt="">
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+        {{-- </li> --}}
+    @endguest
 </div>
