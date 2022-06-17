46ed9fb2bc6f4df2e24d67b752cf1bb162e13df3
abo3adel <abo3adel35@gmail.com>
2022-06-18 00:09:18 +0100

    save theme mode in storage

--- resources/views/layouts/glass.blade.php
+++ resources/views/layouts/glass.blade.php
@@ -22,7 +22,21 @@
 </head>
 
 <body>
-    <div class="dark-light" x-data x-on:click.prevent="() => document.body.classList.toggle('light-mode');">
+    <div class="dark-light" x-data="{
+        darkMode: false,
+        toggle: async function () {
+            this.darkMode = !this.darkMode;
+            localStorage.setItem('darkMode', this.darkMode ? '1' : '0');
+            if (this.darkMode) {
+                document.body.classList.remove('light-mode');
+                return;
+            }
+            document.body.classList.add('light-mode');
+        },
+    }" x-init="async () => {
+        darkMode = localStorage.getItem('darkMode') === '0' ? true : false;
+        toggle();
+    }" x-on:click.prevent="toggle">
         <svg viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5" fill="none" stroke-linecap="round"
             stroke-linejoin="round">
             <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" /></svg>
