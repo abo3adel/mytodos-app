798e157f32c66ed2fc15b2e991b381f9a6168a63
abo3adel <abo3adel35@gmail.com>
2022-06-19 12:52:06 +0100

    styling auth pages

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,8 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+            <div class="app-card" style="width: 100%;color: var(--theme-color)">                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
