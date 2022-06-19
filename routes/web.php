5c55629c062e73d75d2f7766feeb1c6e834e0c08
abo3adel <abo3adel35@gmail.com>
2022-06-19 13:54:39 +0100

    Manually register auth routes
    for some reason it`s not working on production [Auth:routes()]

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Auth::routes();
+// register auth routes manually
+require_once 'auth.php';
 
 Route::get("/", function () {
     return view("welcome");
